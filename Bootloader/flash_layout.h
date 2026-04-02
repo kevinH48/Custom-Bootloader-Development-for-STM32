a652a2de7f7a65eee903901a3c383f980a66b756
Kevin Hingu <kvinh1201@gmail.com>
2026-04-02 19:13:37 +0530

    Add files via upload

---
+++ Bootloader/flash_layout.h
@@ -0,0 +1,17 @@
+/*
+ * flash_layout.h
+ *
+ *  Created on: 03-Mar-2026
+ *      Author: LENOVO
+ */
+
+#ifndef INC_FLASH_LAYOUT_H_
+#define INC_FLASH_LAYOUT_H_
+
+#define BL_START_ADDR 		 0x08000000 //16KB
+#define APP_HEADER_ADDR      0x08004000  // 1KB
+#define APP_START_ADDR       0x08004400
+#define APP_MAX_SIZE		 1024*1024
+
+
+#endif /* INC_FLASH_LAYOUT_H_ */
